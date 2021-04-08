de305bd4f00b601056f89d5f66a5c34bb9626c6a
Alejandro Mery <amery@jpi.io>
2021-04-08 20:34:07 +0100

    Server: add initial NewServer()
    Signed-off-by: Alejandro Mery <amery@jpi.io>

---
+++ server.go
@@ -0,0 +1,24 @@
+package cms
+
+// CMS Server
+type Server struct{}
+
+func NewServer(options ...ServerOption) (*Server, error) {
+	s := &Server{}
+
+	for _ = range options {
+	}
+
+	return s, nil
+}
+
+// Options
+type ServerOption interface{
+	IsServerOption() ServerOption
+}
+
+type serverOption struct{}
+
+func (s *serverOption) IsServerOption() ServerOption {
+	return s
+}
