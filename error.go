6e06708b9a947b60515e02704db251a6db96552e
Alejandro Mery <amery@jpi.io>
2021-04-19 01:01:45 +0000

    error: borrow HandlerError from go.sancus.dev/mix/types
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- error.go
+++ error.go
@@ -1,73 +1,20 @@
 package cms
 
 import (
-	"fmt"
 	"net/http"
+
+	"go.sancus.dev/mix/types"
 )
 
 var (
-	ErrNotFound = &HandlerError{Code: http.StatusNotFound}
+	ErrNotFound = types.ErrNotFound
 )
 
-// Error including HTTP Status and an optional wrapped error
-type Error interface {
-	Error() string
-	Status() int
-	Unwrap() error
-}
-
-// Reference Handler error
-type HandlerError struct {
-	Code int
-	Err  error
-}
-
-func (err HandlerError) Status() int {
-	var code int
-
-	if err.Code != 0 {
-		code = err.Code
-	} else if err.Err == nil {
-		code = http.StatusOK
-	} else {
-		code = http.StatusInternalServerError
-	}
-
-	return code
-}
-
-func (err HandlerError) Unwrap() error {
-	return err.Err
-}
-
-func (err HandlerError) String() string {
-	code := err.Status()
-	text := http.StatusText(code)
-
-	if len(text) == 0 {
-		text = fmt.Sprintf("Unknown Error %d", code)
-	} else if code >= 400 {
-		text = fmt.Sprintf("%s (Error %d)", text, code)
-	}
-
-	return text
-}
-
-func (err HandlerError) Error() string {
-	return err.String()
-}
-
-func (err HandlerError) ServeHTTP(w http.ResponseWriter, _ *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.Header().Set("X-Content-Type-Options", "nosniff")
-	w.WriteHeader(err.Status())
-
-	fmt.Fprintln(w, err)
-	if e := err.Unwrap(); e != nil {
-		fmt.Fprintln(w, "", e)
-	}
-}
+type (
+	Error        = types.Error
+	ErrorHandler = types.ErrorHandler
+	HandlerError = types.HandlerError
+)
 
 // Error Handlers
-type ErrorHandler func(http.ResponseWriter, *http.Request, error)
 type PanicHandler func(http.ResponseWriter, *http.Request, interface{})
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module go.sancus.dev/cms
 
 go 1.16
 
-require github.com/fsnotify/fsnotify v1.4.9
+require (
+	github.com/fsnotify/fsnotify v1.4.9
+	go.sancus.dev/mix v0.0.3
+)
